b93eda5dbb0c08e574fdc67a41c0c3bc7f3faf63
andrerodi <andre.rodi@gmail.com>
2025-10-31 22:57:30 +0100

    Require copilot config in plugin

--- lua/config/copilot.lua
+++ lua/config/copilot.lua
@@ -1,3 +1,2 @@
-vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-.>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 vim.api.nvim_set_keymap("i", "<C-X>", "copilot#Dismiss()", { silent = true, expr = true })
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,5 +2,10 @@ return {
   {
     "github/copilot.vim",
     event = "VimEnter",
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      -- Require configuration from config file
+      require("../config/copilot")
+    end,
   },
 }
