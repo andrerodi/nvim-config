18478e1e8b24704f881db99adced7e30cc2f3b8e
Andre Rodi <andre.rodi@gmail.com>
2026-03-30 22:58:20 +0200

    Changed statuscolumn for folding and absolute/relative line numbers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,14 +2,15 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.number = true
+-- vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.statuscolumn = "%s %= %{v:relnum} %{v:lnum} %C "
-
+-- vim.opt.statuscolumn = "%s %= %{v:relnum} %{v:lnum} %C " -- Show both relative and absolute line numbers
+vim.opt.statuscolumn = "%s %= %{v:relnum ? v:relnum : v:lnum} %C " -- Show relative line numbers, but absolute for the current line
+vim.opt.foldlevel = 99 -- Close all folds below level 1
 vim.opt.foldcolumn = "1" -- Optional: shows the fold column
 vim.opt.fillchars = {
   foldopen = "⌄",
   foldclose = ">",
-  foldsep = " ",
+  foldsep = "|",
   fold = " ",
 }
