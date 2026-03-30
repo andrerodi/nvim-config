bdffbbf9784b9f3a3d9c940be416660a507cbe31
Andre Rodi <andre.rodi@gmail.com>
2026-03-30 22:27:46 +0200

    Make status column right aligned

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.statuscolumn = "%s %{v:relnum} %{v:lnum}"
+vim.opt.statuscolumn = "%s %= %{v:relnum} %{v:lnum} %C "
+
+vim.opt.foldcolumn = "1" -- Optional: shows the fold column
+vim.opt.fillchars = {
+  foldopen = "⌄",
+  foldclose = ">",
+  foldsep = " ",
+  fold = " ",
+}
