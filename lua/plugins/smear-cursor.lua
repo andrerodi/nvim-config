5320ae6fe66b2daecb9c9a4352cda8e9a39b66b1
Andre Rodi <andre.rodi@gmail.com>
2026-03-31 20:39:58 +0200

    Added particles to cursor

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -1,4 +1,9 @@
 return {
   "sphamba/smear-cursor.nvim",
-  opts = {},
+  opts = {
+    particles_enabled = true,
+    particle_max_num = 50,
+    damping = 0.6,
+    gradient_exponent = 0,
+  },
 }
